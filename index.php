b40da5033a94bbfc0de6fdadc5b7af6a9fc303d7
Ekaterina <khirnetkina@mail.com>
2026-06-06 23:51:08 +0800

    добавлена навигационная панель

--- index.php
+++ index.php
@@ -5,9 +5,46 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"  rel="stylesheet" />
 </head>
 <body>
-    <div>Всем привет</div>
-    <div>Версия PHP: <?php print phpversion(); ?></div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#"><i class="fas fa-tv"></i></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/">Главная</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="/movies">Фильмы</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="series">Сериалы</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+            <?php 
+            $url = $_SERVER["REQUEST_URI"];
+
+            echo "Вы на странице: $url, будьте внимательны!<br>";
+
+            if ($url == "/") {
+                require "views/main.php";
+            } elseif ($url == "/movies") {
+                require "views/movies.php";
+            } elseif ($url == "/series") {
+                require "views/series.php";
+            } 
+            ?>
+    </div>
 </body>
 </html>
