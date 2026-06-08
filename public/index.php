c3b2e7e9ca27a1b137a3e219c77c8c07dfc9c3de
Ekaterina <khirnetkina@mail.com>
2026-06-08 14:53:44 +0800

    выполнила 4 задачу из CRUD

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ require_once '../framework/autoload.php';
 require_once "../controllers/MainController.php";
 require_once "../controllers/Controller404.php";
 require_once "../controllers/ObjectController.php";
+require_once "../controllers/SearchController.php"; 
 
 $loader = new \Twig\Loader\FilesystemLoader('../views');
 
@@ -27,4 +28,5 @@ $router->add("/movies", MoviesController::class);
 $router->add("/m_s/(?P<id>\d+)", ObjectController::class);
 $router->add("/m_s/(?P<id>\d+)?show=image", ObjectController::class); 
 $router->add("/m_s/(?P<id>\d+)?show=info", ObjectController::class);  
+$router->add("/search", SearchController::class);
 $router->get_or_default(Controller404::class);
